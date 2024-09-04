ffc4d800c616b1d86d3cad5721bcbcbb80544ab6
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-09-04 22:21:15 +0800

    Deployed baf832c with MkDocs version: 1.0.4

---
+++ javascripts/analytics.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-FHJ5Z3WRR5';
+script.async = true;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-FHJ5Z3WRR5');
